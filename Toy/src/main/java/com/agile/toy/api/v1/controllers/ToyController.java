4d18cc2932e2c70abb9152cee569d743299efa35
blackwidow <blackwidow@blackwidows-Mac-mini.local>
2018-03-01 11:18:03 +0700

    allow cross origin

--- Toy/src/main/java/com/agile/toy/api/v1/controllers/ToyController.java
+++ Toy/src/main/java/com/agile/toy/api/v1/controllers/ToyController.java
@@ -20,20 +20,20 @@ public class ToyController {
         this.toyService = toyService;
     }
 
-    @CrossOrigin(origins = "128.199.190.215:8080")
+    @CrossOrigin(origins = "*")
     @GetMapping
     public String hello(){
         return "hello world";
     }
 
-    @CrossOrigin(origins = "128.199.190.215:8080")
+    @CrossOrigin(origins = "*")
     @GetMapping("/toyList")
     @ResponseStatus(HttpStatus.OK)
     public ToyListDTO getToyListByGenderAndAge(@RequestParam String gender, @RequestParam String age){
         return new ToyListDTO(toyService.getToyFromGenderAndAge(gender,age));
     }
 
-    @CrossOrigin(origins = "128.199.190.215:8080")
+    @CrossOrigin(origins = "*")
     @GetMapping("/toy/{id}")
     @ResponseStatus(HttpStatus.OK)
     public ToyDetailsDTO getToyDetailsById(@PathVariable Long id){
